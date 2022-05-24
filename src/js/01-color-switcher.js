f76aef08676c90e9b2b909f3755b6a83cdb7a3a4
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-24 21:07:22 +0300

    T1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+const refs = {
+    btnStartColor: document.querySelector('[data-start]'),
+    body: document.querySelector("body"),
+    btnStopColor: document.querySelector('[data-stop]'),
+  }
+  let intervalId = null;
+  refs.btnStartColor.addEventListener("click", startChangeColor);
+  refs.btnStopColor.addEventListener("click", stopChangeColor);
+  
+  
+  function startChangeColor() {
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+        }, 1000);
+    showDisabledAtributeStartBtn();
+    hideDisabledAtributeStopBtn();
+  }
 
+  function stopChangeColor() {
+       clearInterval(intervalId);
+       hideDisabledAtributeStartBtn();
+       showDisabledAtributeStopBtn();
+
+  }
+    
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+  }
+function showDisabledAtributeStartBtn() {
+    refs.btnStartColor.setAttribute('disabled', '');
+} //ф-ція встановлення атрибуту disabled на кнопку старт
+
+function showDisabledAtributeStopBtn() {
+    refs.btnStopColor.setAttribute('disabled', '');
+} //ф-ція встановлення атрибуту disabled на кнопку стоп
+
+function hideDisabledAtributeStartBtn() {
+    refs.btnStartColor.removeAttribute('disabled');
+}//ф-ція зняття атрибуту disabled з кнопки старт
+
+function hideDisabledAtributeStopBtn() {
+    refs.btnStopColor.removeAttribute('disabled');
+}//ф-ція зняття атрибуту disabled з кнопки стоп
