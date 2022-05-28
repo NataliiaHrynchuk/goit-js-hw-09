46842cddbae50d71a5d3d6b6b52b02064e7ad31b
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-28 11:34:22 +0300

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,13 +3,18 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 console.log(flatpickr);
+let selectedDate = null;
 
 const options = {
     enableTime: true,
+    dateFormat: 'Y-m-d H:i',
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+      // console.log(selectedDates[0]);
+      selectedDate = selectedDates[0];
+      console.log(selectedDate);
     },
   };
+  flatpickr("#datetime-picker", options);
